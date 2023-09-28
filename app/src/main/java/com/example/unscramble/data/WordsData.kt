86efb86a11d054434e1d7289fbb008d422956b84
Chakshu <chakshuharsh97@gmail.com>
2023-09-28 15:13:24 +0530

    Comment 1.0

--- app/src/main/java/com/example/unscramble/data/WordsData.kt
+++ app/src/main/java/com/example/unscramble/data/WordsData.kt
@@ -20,7 +20,7 @@ const val MAX_NO_OF_WORDS = 10
 const val SCORE_INCREASE = 20
 
 // Set with all the words for the Game
-val allWords: Set<String> =
+val allWords: Set<String> =// Set will contain unique elements and setOf -> creates an immutable list
     setOf(
         "animal",
         "auto",
